a7ce8744ad8b16bd536a72f9ff8a47097acf15c9
Hulford <moraleshulford@gmail.com>
2023-11-24 10:52:47 -0500

    UpperCase, LowerCase y TitleCase Pipes

--- src/app/primeNg/primeNg.module.ts
+++ src/app/primeNg/primeNg.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
-import { MenuModule } from 'primeng/menu';
+import {  ButtonModule } from 'primeng/button';
+//import { MenuModule } from 'primeng/menu';
 import { MenubarModule} from 'primeng/menubar'
+import {CardModule} from 'primeng/card'
+import {FieldsetModule} from 'primeng/fieldset'
+import{ PanelModule} from 'primeng/panel'
 
 
 
 @NgModule({
 exports:[
-  MenuModule,
-  MenubarModule
+  //MenuModule,
+  MenubarModule,
+  ButtonModule,
+  CardModule,
+  FieldsetModule,
+  MenubarModule,
+  PanelModule
+
 ]
 })
 export class PrimeNgModule { }
