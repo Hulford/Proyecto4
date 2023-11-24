d8fbb94f59fad63e40eaf435d823a4f9c6ffa83c
Hulford <moraleshulford@gmail.com>
2023-11-24 15:55:40 -0500

    Decimal, Currency y Percent Pipes

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./numbers-page.component.css']
 })
 export class NumbersPageComponent {
-
+public totalSells: number  = 25384783.5567;
+public percent: number  = 0.5065;
 }
