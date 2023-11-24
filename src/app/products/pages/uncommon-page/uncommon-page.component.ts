5a08bed0523d1dedb67b2f574b516378ebba3360
Hulford <moraleshulford@gmail.com>
2023-11-24 16:18:21 -0500

    i18nSelectPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = 'Hulford';
+  public gender: 'male' | 'female' = 'male';
+ public invitationMap = {
+  'male': 'invitarlo',
+  'female': 'invitarla'
+ }
+  changeClient():void{
+    this.name= 'melissa';
+    this.gender= 'female';
+  }
+
 }
