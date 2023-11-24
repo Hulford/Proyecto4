5ebcacd6ae78ef69c41fefffde78f12dbbbd6a0b
Hulford <moraleshulford@gmail.com>
2023-11-24 09:54:49 -0500

    PrimerNG - MenuBar

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -14,18 +14,32 @@ public menuItems: MenuItem[]=[];
 
 ngOnInit() {
   this.menuItems = [
-      {
-          label: 'New',
-          icon: 'pi pi-fw pi-plus',
-      },
-      {
-          label: 'Open',
-          icon: 'pi pi-fw pi-download'
-      },
-      {
-        label: 'Undo',
-        icon: 'pi pi-fw pi-refresh'
+    {
+      label: 'Pipes de Angular',
+    icon:'pi pi-desktop',
+  items: [
+    {
+    label:'Textos y Fechas',
+    icon:'pi pi-align-left'
+  }, {
+    label:'Numeros',
+    icon:'pi pi-dollar'
+  }, {
+    label:'No comunes',
+    icon:'pi pi-globe',
+  },
+]
+},{
+  label:'Pipes personalizados',
+  icon:'pi pi-cog',
+  items: [
+    {
+      label: 'Otro elemento',
+      icon: 'pi pi-cog',
+
     }
+  ]
+}
   ];
 }
 
